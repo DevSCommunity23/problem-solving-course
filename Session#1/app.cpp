098df2b9ed1efe850a4ecdb43a091d359269c07a
Hamza <hamzahassanain067@gmail.com>
2024-04-15 20:06:13 +0200

    hamza added first session

---
+++ Session#1/app.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    // int a = 10;       // take values from -2000000000 to 2000000000
+    // long long b = 23; // take values from -9000000000000000000 to 9000000000000000000
+    // char c = 'A';
+    // char d = 65; // takes values from -128 to 127
+    // string e = "Hello";
+    // // unsigned when put before the data type, it will only take positive values
+    // unsigned int x = 10;   // 0 -> 18 * 1000000000000000000
+    // long double aa = 10.0; // 100 *1000000000000000000 not recommended, very very slow
+
+    // a = 20 + 10;
+
+    // aa = x + a;
+    // int X = 99;
+    // int remidner = X % 2; // 1
+
+    // string name = "Ali";
+    // string lastName = "Khan";
+    // string fullName = name + " " + lastName;
+
+    // a += 10;                // a = a + 10;
+    // a -= 10;                // a = a - 10;
+    // a *= 10;                // a = a * 10;
+    // a /= 10;                // a = a / 10;
+    // a %= 5;                 // a = a % 5;
+    // name += " " + lastName; // name = name + " " + lastName;
+
+    // cout << name << endl;
+
+    /// read input from file, user, console => standered input stream
+
+    // string x;
+
+    // getline(cin, x); // reads a whole line
+
+    // cout << "The value of x is: " << x << endl;
+
+    // int a;
+    // cin >> a;
+    // cout << "The value of a is: " << a << endl;
+
+    // double somedouble;
+    // cin >> somedouble;
+    // cout << fixed << setprecision(2) << somedouble << endl;
+
+    /*
+        simple programe to
+        1. take two numbers from the user
+        2. add them
+        3. print the result
+
+        this is a commented part
+    */
+
+    // int a, b;
+    // cout << "Enter the first number: ";
+    // cin >> a;
+    // cout << "Enter the second number: ";
+    // cin >> b;
+
+    // int result = a + b;
+    // cout << "The result is: " << result << endl;
+
+    // int a, b;
+    // cout << "Enter the two numbers: ";
+    // cin >> a >> b;
+
+    // int result = a + b;
+    // cout << "The result is: " << result << endl;
+
+    return 0;
+}
